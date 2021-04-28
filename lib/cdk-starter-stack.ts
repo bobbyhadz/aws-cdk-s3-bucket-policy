ae3d772fd1c91abc888acd83629df7708d2a9b23
Borislav Hadzhiev <opmftw@gmail.com>
2021-04-28 18:01:35 +0300

    updated infra

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -6,14 +6,47 @@ export class CdkStarterStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    /**
+     * Approach 1 - implicit with `addToResourcePolicy`
+     */
+
     // 👇 create the s3 bucket
-    const bucket = new s3.Bucket(this, 'bucket-id', {
+    const bucket1 = new s3.Bucket(this, 'bucket-id-1', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // 👇 `addToResourcePolicy` creates a Bucket Policy automatically
+    bucket1.addToResourcePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        principals: [new iam.ServicePrincipal('lambda.amazonaws.com')],
+        actions: ['s3:GetObject'],
+        resources: [`${bucket1.bucketArn}/*`],
+      }),
+    );
+
+    // 👇 access the bucket policy
+    bucket1.policy?.document.addStatements(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        principals: [new iam.ServicePrincipal('lambda.amazonaws.com')],
+        actions: ['s3:GetBucketTagging'],
+        resources: [bucket1.bucketArn],
+      }),
+    );
+
+    /**
+     * Approach 2 - explicit with `BucketPolicy`
+     */
+
+    // 👇 create the s3 bucket
+    const bucket2 = new s3.Bucket(this, 'bucket-id-2', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
     // 👇 create the bucket policy
-    const bucketPolicy = new s3.BucketPolicy(this, 'bucket-policy-id', {
-      bucket,
+    const bucketPolicy = new s3.BucketPolicy(this, 'bucket-policy-id-2', {
+      bucket: bucket2,
     });
 
     // 👇 add policy statements ot the bucket policy
@@ -22,7 +55,7 @@ export class CdkStarterStack extends cdk.Stack {
         effect: iam.Effect.ALLOW,
         principals: [new iam.ServicePrincipal('lambda.amazonaws.com')],
         actions: ['s3:GetObject'],
-        resources: [`${bucket.bucketArn}/*`],
+        resources: [`${bucket2.bucketArn}/*`],
       }),
     );
   }
